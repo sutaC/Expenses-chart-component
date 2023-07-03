4b6e9456c867bc3f963b0cf2e9a493c42124035d
Antoni Siekański <catus.pl@gmail.com>
2023-07-03 16:22:04 +0200

    Html layout

---
+++ src/lib/components/chart.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let data: Array<any>;
+</script>
+
+<div class="container">
+	<div class="collumn">
+		<div class="data" />
+		<p class="heading">{'a'}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Chart from '$lib/components/chart.svelte';
+</script>
+
+<header>
+	<h1>My balance</h1>
+
+	<p>$921.48</p>
+</header>
+
+<main>
+	<section>
+		<h2>Spending - Last 7 days</h2>
+		<Chart data={[]} />
+	</section>
+	<section>
+		<div class="total">
+			<h2>Total this month</h2>
+			<p>$478.33</p>
+		</div>
+		<div class="comparison">
+			<p>+2.4%</p>
+			<p>from last month</p>
+		</div>
+	</section>
+</main>
+
+<footer>
+	Challenge by <a
+		href="https://www.frontendmentor.io?ref=challenge"
+		target="_blank"
+		rel="noopener noreferrer">Frontend Mentor</a
+	>. Coded by
+	<a href="https://www.frontendmentor.io/profile/sutaC" target="_blank" rel="noopener noreferrer"
+		>sutaC</a
+	>.
+</footer>
